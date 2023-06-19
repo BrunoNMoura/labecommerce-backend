a7ecaa6b6c04036236e4d1f2d4537407eff13f4d
BrunoNMoura <bnmalemao@gmail.com>
2023-06-19 19:56:15 -0300

    Validações dos endpoints mais criação do postman realizado com sucesso

--- src/database.ts
+++ src/database.ts
@@ -19,14 +19,14 @@ export const users: TUser[] = [
 
 export const products: TProduct[] = [
   {
-    id: "prod001",
+    id: "p001",
     name: "Mouse gamer",
     price: 250,
     description: "Melhor mouse do mercado!",
     imageUrl: "https://picsum.photos/seed/Mouse%20gamer/400",
   },
   {
-    id: "prod002",
+    id: "p002",
     name: "Monitor",
     price: 900,
     description: "Monitor LED Full HD 24 polegadas",
